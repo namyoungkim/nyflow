2b59997821c9a6e9e882de9fefcd2f9399b09874
leo <liniarq@gmail.com>
2021-10-10 18:29:40 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
  
 setup(
     name = 'nyflow',
-    version = '0.0.3',
+    version = '0.0.5',
     description = 'nyfow is deeplearning framwork',
     author = 'namyoungKim',
     author_email = 'liniarq@gmail.com',

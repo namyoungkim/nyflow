36c9c23fbadd7df8e39bd26c12810192d293eec8
leo <liniarq@gmail.com>
2021-10-11 22:31:36 +0900

    add functions.py

---
+++ nyflow/functions.py
@@ -0,0 +1,38 @@
+import numpy as np
+import dezero
+# from nyflow import cuda, utils
+from nyflow.core import Function, Variable, as_variable, as_array
+
+
+# =============================================================================
+# Basic functions: sin / cos / tanh / exp / log
+# =============================================================================
+class Sin(Function):
+    def forward(self, x):
+        y = np.sin(x)
+        return y
+
+
+    def backward(self, gy):
+        x, = self.inputs
+        gx = gy * cos(x)    # 1.
+        return gx
+
+
+def sin(x):
+    return Sin()(x)
+
+
+class Cos(Function):
+    def forward(self, x):
+        y = np.cos(x)
+        return y
+
+    def backward(self, gy):
+        x, = self.inputs
+        gx = gy * -sin(x)
+        return gx
+
+
+def cos(x):
+    return Cos()(x)

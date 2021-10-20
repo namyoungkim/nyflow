e93ad888af0d2a674250ae9b95ff9d208211f72a
leo <liniarq@gmail.com>
2021-10-20 23:29:59 +0900

    update functions.py

--- nyflow/functions.py
+++ nyflow/functions.py
@@ -12,12 +12,10 @@ class Sin(Function):
         y = np.sin(x)
         return y
 
-
     def backward(self, gy):
         x, = self.inputs
         gx = gy * cos(x)    # 1.
         return gx
-
 
 def sin(x):
     return Sin()(x)
@@ -33,7 +31,6 @@ class Cos(Function):
         gx = gy * -sin(x)
         return gx
 
-
 def cos(x):
     return Cos()(x)
 
@@ -47,7 +44,6 @@ class Tanh(Function):
         y = self.outputs[0]()  # weakref
         gx = gy * (1 - y * y)
         return gx
-
 
 def tanh(x):
     return Tanh()(x)
@@ -63,7 +59,6 @@ class Exp(Function):
         gx = gy * y
         return gx
 
-
 def exp(x):
     return Exp()(x)
 
@@ -78,6 +73,48 @@ class Log(Function):
         gx = gy / x
         return gx
 
-
 def log(x):
     return Log()(x)
+
+
+# =============================================================================
+# Tensor operations: reshape / transpose / get_item / expand_dims / flatten
+# =============================================================================
+class Reshape(Function):
+    def __init__(self, shape):
+        self.shape = shape      ### 클래스를 초기화할 때 변형 목표가 되는 형상을 shape 인수로 받는다.
+
+    def forward(self, x):
+        self.x_shape = x.shape      ### 입력 x의 형상을 기억해둔다.
+        y = x.reshape(self.shape)
+        return y
+
+    def backward(self, gy):
+        return reshape(gy, self.x_shape)
+
+
+def reshape(x, shape):
+    if x.shape == shape:
+        return as_variable(x)
+    return Reshape(shape)(x)
+
+
+class Transpose(Function):
+    def __init__(self, axes=None):
+        self.axes = axes
+
+    def forward(self, x):
+        y = x.transpose(self.axes)
+        return y
+
+    def backward(self, gy):
+        if self.axes is None:
+            return transpose(gy)
+
+        axes_len = len(self.axes)
+        inv_axes = tuple(np.argsort([ax % axes_len for ax in self.axes]))
+        return transpose(gy, inv_axes)
+
+
+def transpose(x, axes=None):
+    return Transpose(axes)(x)
